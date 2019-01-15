1d26fa95e52ca9812a5404c8136f51f99e7b3818
zdq0394 <zdq123.hn@163.com>
2019-01-15 15:23:03 +0800

    refactor

--- operator/consul/handler.go
+++ operator/consul/handler.go
@@ -17,23 +17,27 @@ var defaultLabels = map[string]string{
 }
 
 const (
-	ConsulLabelKey = "consul"
+	// InstanceLabelKey its value is the name of consul.consul.zdq0394.io resouce.
+	InstanceLabelKey = "ConsulInstance"
 )
 
-type ConsulHandler struct {
+// Handler handles events of add/update/delete of Consul Resouce.
+type Handler struct {
 	Manager mgr.ConsulManager
 	Labels  map[string]string
 }
 
-func NewConsulHandler(labels map[string]string, manager mgr.ConsulManager) *ConsulHandler {
+// NewConsulHandler return a Handler instance.
+func NewConsulHandler(labels map[string]string, manager mgr.ConsulManager) *Handler {
 	curLabels := util.MergeLabels(defaultLabels, labels)
-	return &ConsulHandler{
+	return &Handler{
 		Labels:  curLabels,
 		Manager: manager,
 	}
 }
 
-func (h *ConsulHandler) Add(ctx context.Context, obj runtime.Object) error {
+// Add will be called when a Consul resource added/updated.
+func (h *Handler) Add(ctx context.Context, obj runtime.Object) error {
 	log.Infoln("Create Consul Here...")
 	rc, ok := obj.(*v1alpha1.Consul)
 	if !ok {
@@ -46,27 +50,27 @@ func (h *ConsulHandler) Add(ctx context.Context, obj runtime.Object) error {
 	return h.ensurePresent(rc, labels, oRefs)
 }
 
-func (h *ConsulHandler) Delete(ctx context.Context, key string) error {
+// Delete will be called when a Consul resource deleted.
+func (h *Handler) Delete(ctx context.Context, key string) error {
 	log.Infoln("Delete Consul Here:", key)
-	// No need to do anything, it will be handled by the owner reference done
-	// on the creation.
+	// No need to do anything, Kubernetes will handle it by the owner reference done on the creation.
 	return nil
 }
 
-func (h *ConsulHandler) createOwnerReferences(rc *v1alpha1.Consul) []metav1.OwnerReference {
+func (h *Handler) createOwnerReferences(rc *v1alpha1.Consul) []metav1.OwnerReference {
 	rcvk := v1alpha1.VersionKind(v1alpha1.RCKind)
 	return []metav1.OwnerReference{
 		*metav1.NewControllerRef(rc, rcvk),
 	}
 }
 
-func (h *ConsulHandler) generateInstanceLabels(rc *v1alpha1.Consul) map[string]string {
+func (h *Handler) generateInstanceLabels(rc *v1alpha1.Consul) map[string]string {
 	return map[string]string{
-		ConsulLabelKey: rc.Name,
+		InstanceLabelKey: rc.Name,
 	}
 }
 
-func (h *ConsulHandler) ensurePresent(rc *v1alpha1.Consul,
+func (h *Handler) ensurePresent(rc *v1alpha1.Consul,
 	labels map[string]string, ownerRefs []metav1.OwnerReference) error {
 	if err := h.Manager.EnsureConsulConfigMap(rc, labels, ownerRefs); err != nil {
 		return err
