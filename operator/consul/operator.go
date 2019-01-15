2aa914564bef23d77a312743b638787e893b6922
zdq0394 <zdq123.hn@163.com>
2019-01-15 16:47:44 +0800

    refactor

--- operator/consul/operator.go
+++ operator/consul/operator.go
@@ -2,9 +2,9 @@ package consul
 
 import (
 	mgr "github.com/zdq0394/consul-operator/operator/consul/handler"
-	k8sclient "github.com/zdq0394/consul-operator/pkg/k8s"
+	"github.com/zdq0394/consul-operator/pkg/k8sutil"
 	"github.com/zdq0394/consul-operator/pkg/log"
-	k8service "github.com/zdq0394/consul-operator/service/k8s"
+	"github.com/zdq0394/consul-operator/service/k8s"
 	"github.com/zdq0394/k8soperator/pkg/operator"
 	"github.com/zdq0394/k8soperator/pkg/operator/controller"
 )
@@ -18,12 +18,12 @@ type Config struct {
 
 // Start the Operator
 func Start(conf *Config) error {
-	kubeClient, consulClient, aeClient, _ := k8sclient.CreateKubernetesClients(conf.Development, conf.Kubeconfig)
+	kubeClient, consulClient, aeClient, _ := k8sutil.CreateKubernetesClients(conf.Development, conf.Kubeconfig)
 	logger := log.Base()
-	kubeService := k8service.New(kubeClient, consulClient, aeClient, logger)
+	k8sService := k8s.New(kubeClient, consulClient, aeClient, logger)
 
-	crd := NewConsulCRD(kubeService)
-	manager := mgr.NewConsulManager(kubeService, conf.ClusterDomain)
+	crd := NewConsulCRD(k8sService)
+	manager := mgr.NewConsulManager(k8sService, conf.ClusterDomain)
 	handler := NewConsulHandler(nil, manager)
 
 	controllerCfg := &controller.Config{
